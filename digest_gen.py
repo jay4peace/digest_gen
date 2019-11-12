12060bc5adccddef76a3c7d81074964b81f800ea
jay4peace <42663367+jay4peace@users.noreply.github.com>
2019-11-12 12:56:33 +0900

    update digest_gen.py

--- digest_gen.py
+++ digest_gen.py
@@ -2,19 +2,23 @@ import argparse
 from hashlib import md5
 
 def gen_digest(user, password, realm):
-    digest = md5(':'.join((user, realm, password)).encode('utf-8')).hexdigest()
-    htdigest = ':'.join((user, realm, digest))
-    print(htdigest)
+    return md5(':'.join((user, realm, password)).encode('utf-8')).hexdigest()
 
 def main():
     parser = argparse.ArgumentParser(description='Generating htdigest.')
     parser.add_argument('-u', '--user', required=True)
     parser.add_argument('-p', '--password', required=True)
     parser.add_argument('-r', '--realm', default='default')
+    parser.add_argument('-f', '--form', default='htdigest',
+        help='result print format(htdigest or tizen)')
 
     args = parser.parse_args()
 
-    gen_digest(args.user, args.password, args.realm)
+    digest = gen_digest(args.user, args.password, args.realm)
+    if args.form == "tizen":
+        print('='.join((args.user, digest)))
+    else :
+        print(':'.join((args.user, args.realm, digest)))
 
 if __name__=="__main__":
     main()
